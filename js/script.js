90a861d2e32cedfdb8aef25f95d23f6dd1b7b695
bra1nf00d <pixeljack3@gmail.com>
2020-12-01 13:56:32 +0200

    delete

--- js/script.js
+++
@@ -1,7 +0,0 @@
-let list = document.querySelector('ul');
-let li = list.querySelectorAll('li');
-for (let i = 0; i < li.length; i++) {
-	li[i].addEventListener('click', () => {
-		li[i].classList.toggle('crossOut');
-	});
-}
